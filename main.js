9500b2df6ed74d0b8b49d3503f87dd03977e7d97
bulvasmartin05-coder <bulvasmartin05@gmail.com>
2026-05-13 13:58:48 +0200

    Add main.js - Electron main process

---
+++ main.js
@@ -0,0 +1,82 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      sandbox: true,
+    },
+    icon: path.join(__dirname, 'assets', 'icon.png'),
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadFile(path.join(__dirname, 'src', 'index.html'));
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// Menu
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Exit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => app.quit(),
+      },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+    ],
+  },
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
